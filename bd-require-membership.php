6fea9246ab7aa888d49dccc48763521ce6bfd036
Matt Richmond <matt@bigdawggi.com>
2013-05-06 13:34:31 -0600

    adding Changelog, license info to plugin file, and ver bump to 1.1

--- bd-require-membership.php
+++ bd-require-membership.php
@@ -2,9 +2,10 @@
 /*
 Plugin Name: Require Membership
 Description: Crazy-simple way to require people to log in to view your content.
-Version: 1.0
+Version: 1.1
 Author: Bigdawggi
 Author URI: http://bigdawggi.com
+License: GPLv2 or later
 */
 
 function bd_require_membership() {
